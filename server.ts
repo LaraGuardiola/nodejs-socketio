eec35cd135ccc1babfc7ef6b40905dc7c0144220
Sergio <webdev.slara@gmail.com>
2023-01-20 23:27:19 +0100

    Added onlineCount emission

--- server.ts
+++ server.ts
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 const io = new Server({ 
   cors: {
     origin: "https://angular-clientio.vercel.app",
+    // origin: "http://localhost:4200",
     methods: ["GET", "POST"]
   }
 })
@@ -13,13 +14,21 @@ const io = new Server({
 //on connection
 io.on('connection', (socket: Socket) => {
   console.log(`${socket.id} has connected - ${new Date()}`)
+  setOnlineCount(socket)
 
   //on message
   socket.on("message", (arg: string) => {
-    console.log(arg);
+    console.log(`${new Date()}: ${arg}`);
     socket.broadcast.emit("response", arg)
   });
 })
+
+//sends connected clients
+const setOnlineCount = (socket: Socket) => {
+  setInterval(() => {
+    socket.broadcast.emit("onlineCount", io.engine.clientsCount)
+  }, 1000)
+}
 
 const server = () => {
   io.listen(PORT as number)
